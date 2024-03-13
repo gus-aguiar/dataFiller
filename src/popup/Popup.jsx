3e97e42b63655081bc5764560f61450865276ec5
gus-aguiar <gustavopma90@gmail.com>
2024-03-12 22:10:12 -0300

    tabalhando na logica de setAlreadyInputs

--- src/popup/Popup.jsx
+++ src/popup/Popup.jsx
@@ -9,22 +9,26 @@ export const Popup = () => {
   const [allInputs, setAllInputs] = useState([])
   const [hasCapturedInputs, setHasCapturedInputs] = useState(false);
   const [selectedKeys, setSelectedKeys] = useState({});
+  const [catchInputs, setCatchInputs] = useState(false);
+
+  // useEffect(() => {
+  //   // Quando a aplicação carrega, verifica se as chaves selecionadas estão no localStorage
+  //   const storedKeys = localStorage.getItem('selectedKeys');
+  //   if (storedKeys) {
+  //     setSelectedKeys(JSON.parse(storedKeys)); // Se existir, seta selectedKeys com essas chaves
+  //   }
+  // }, []);
 
 
-  useEffect(() => {
-    // Quando a aplicação carrega, verifica se as chaves selecionadas estão no localStorage
-    const storedKeys = localStorage.getItem('selectedKeys');
-    if (storedKeys) {
-      setSelectedKeys(JSON.parse(storedKeys)); // Se existir, seta selectedKeys com essas chaves
-    }
-  }, []);
+
 
   const capturaInputs = async () => {
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    setCatchInputs(true)
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: function getAllInputs() {
-        const allInputs = Array.from(document.querySelectorAll('input')).map(input => {
+        const allInputs = Array.from(document.querySelectorAll('input')).map((input, index) => {
           const hash = 'a' + Math.random().toString(36).substring(2, 15); // Adiciona 'a' antes do hash
           input.classList.add(hash);
           return {
@@ -38,10 +42,12 @@ export const Popup = () => {
             placeholder: input.placeholder,
             label: input.label,
             selectedFakeData: '',
+            index,
             // Adicione aqui qualquer outra propriedade que você precise
           };
         });
         return allInputs; // Retorna a lista de objetos
+
       },
     }, (results) => {
       if (chrome.runtime.lastError) {
@@ -49,7 +55,6 @@ export const Popup = () => {
       } else {
         setHasCapturedInputs(true);
         setAllInputs(results[0].result); // Define o estado com o resultado
-        console.log(results[0].result);
         localStorage.setItem('inputClasses', JSON.stringify(results[0].result)); // Salva os resultados no localStorage
       }
     });
@@ -57,25 +62,60 @@ export const Popup = () => {
 
   const setInputs = async () => {
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    (allInputs.length === 0) && setAllInputs(JSON.parse(localStorage.getItem('inputClasses'))); // Se não houver inputs, recupera do localStorage
 
     allInputs.forEach(input => {
       const inputValue = fakeData()[input.selectedFakeData] || '';
 
-      console.log(input.uniqueClass, fakeData()[input.selectedFakeData]);
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: (uniqueClass, inputValue) => {
-          const inputElement = document.querySelector(`input[class*="${uniqueClass}"]`);
+        function: (inputIndex, inputValue) => {
+          const allPageInputs = Array.from(document.querySelectorAll('input'));
+          const inputElement = allPageInputs[inputIndex];
           if (inputElement && inputValue) {
             inputElement.value = inputValue;
             let event = new Event("input", { bubbles: true });
             inputElement.dispatchEvent(event);
           }
-          else { return console.log('Não foi possível preencher o input: ' + uniqueClass + ' com o valor: ' + inputValue) }
+          else { return console.log('Não foi possível preencher o input: ' + inputIndex + ' com o valor: ' + inputValue) }
         },
-        args: [input.uniqueClass, inputValue] // Usa a classe única
+        args: [input.index, inputValue]
       });
+    });
+  }
+
+  const setAlreadyInputs = async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    // Se não houver inputs, recupera do localStorage
+    if (allInputs.length === 0) {
+      setAllInputs(JSON.parse(localStorage.getItem('inputClasses')));
+    }
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: (allInputs, fakeData) => {
+        // Captura todos os inputs da página
+        const allPageInputs = Array.from(document.querySelectorAll('input'));
+        allInputs.forEach(input => {
+          const inputValue = fakeData[input.selectedFakeData] || 'xablau';
+          console.log('selectedFakeData:', input.selectedFakeData);
+          console.log('fakeData:', fakeData());
+
+          // Encontra o input correspondente na página pelo índice
+          const inputElement = allPageInputs[input.index];
+          console.log(inputElement);
+
+          if (inputElement && inputValue) {
+            inputElement.value = inputValue;
+            let event = new Event("input", { bubbles: true });
+            inputElement.dispatchEvent(event);
+          }
+          else {
+            console.log('Não foi possível preencher o input: ' + input.index + ' com o valor: ' + inputValue);
+          }
+        });
+
+      },
+      args: [allInputs, fakeData()]
     });
   }
   const handleInputChange = (event, index) => {
@@ -101,10 +141,14 @@ export const Popup = () => {
         </button>
 
         <button onClick={() => setInputs()}>
-          Preencher inputs
+          Preencher inputs novos
+        </button>
+
+        <button onClick={() => setAlreadyInputs()}>
+          Preencher inputs já capturados
         </button>
       </div>
-      {allInputs.map((input, index) => (
+      {catchInputs && allInputs.map((input, index) => (
         <InputDropdown
           key={input.className}
           input={input}
