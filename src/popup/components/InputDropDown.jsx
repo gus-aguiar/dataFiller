24941c73197d126d764a75ff733d05306ab1507a
iurymagano <iurymagano17@live.com>
2024-04-03 20:15:38 -0300

    fix: change style inputs

--- src/popup/components/InputDropDown.jsx
+++ src/popup/components/InputDropDown.jsx
@@ -1,54 +1,74 @@
-import { fakeData } from '../helpers/fakeData';
+import { fakeData } from "../helpers/fakeData";
 
 function InputDropdown({ input, onChange }) {
-  const label = input.label || input.placeholder || input.innerText || input.id || input.className;
-
+  const label =
+    input.label ||
+    input.placeholder ||
+    input.innerText ||
+    input.id ||
+    input.className;
 
   const colorInputs = async (selectedClass) => {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: (selectedClass) => {
-        const inputElement = document.querySelector(`input[class*="${selectedClass}"]`);
+        console.log(selectedClass);
+        const inputElement = document.querySelector(
+          `input[class*="${selectedClass}"]`
+        );
         if (inputElement) {
-          inputElement.style.border = '2px solid blue'; // Adiciona uma borda azul ao input
+          inputElement.style.border = "2px solid blue"; // Adiciona uma borda azul ao input
         }
       },
-      args: [selectedClass] // Usa a classe única
+      args: [selectedClass], // Usa a classe única
     });
-  }
+  };
 
   const removeBorder = async (selectedClass) => {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: (selectedClass) => {
-        const inputElement = document.querySelector(`input[class*="${selectedClass}"]`);
+        const inputElement = document.querySelector(
+          `input[class*="${selectedClass}"]`
+        );
         if (inputElement) {
-          inputElement.style.border = ''; // Remove a borda do input
+          inputElement.style.border = ""; // Remove a borda do input
         }
       },
-      args: [selectedClass] // Usa a classe única
+      args: [selectedClass], // Usa a classe única
     });
-  }
+  };
 
   return (
     <div key={input.className}>
-      <p>{label}</p>
-      <select
-        value={input.selectedFakeData || ''}
-        onChange={onChange}
-      >
-        {Object.keys(fakeData()).map(key => (
-          <option key={key} value={key}>{key}</option>
-        ))}
-      </select>
-      <button
-        onClick={() => colorInputs(input.className)}
-        onMouseLeave={() => removeBorder(input.className)}
-      >
-        Selecionar
-      </button>
+      <span>{label || "Sem label"}</span>
+      <div style={{ display: "flex", alignItems: "center", gap: ".4rem" }}>
+        <select
+          value={input.selectedFakeData || ""}
+          onChange={onChange}
+          style={{ padding: "10px 10px" }}
+        >
+          {Object.keys(fakeData()).map((key) => (
+            <option key={key} value={key}>
+              {key}
+            </option>
+          ))}
+        </select>
+        <button
+          onClick={() => colorInputs(input.className)}
+          onMouseLeave={() => removeBorder(input.className)}
+        >
+          Selecionar
+        </button>
+      </div>
     </div>
   );
 }
